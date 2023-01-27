bc9b6efa0e32e1d66bde2ec117fd0fb66a9491a7
ri-gh <98031978+ri-gh@users.noreply.github.com>
2023-01-27 15:54:15 +0100

    Update mru.py

--- mru.py
+++ mru.py
@@ -5,6 +5,10 @@ import requests
 import streamlit as st
 from datetime import timedelta
 import os
+import logging
+import scrapy
+from scrapy import cmdline
+from scrapy.crawler import CrawlerProcess 
 
 st.set_page_config(
     page_title="Hotel analysis for Mauritius Island",
@@ -223,21 +227,146 @@ Now pick the city you'd rather stay to after the weather analysis and find your 
 #due to multithread issue with streamlit we have to use 'os' module
 #to put signal on the main thread
 @st.cache
-def os_use():
-    os.system('python run.py')
-    return pd.read_json('src/All_Booking_mru.json')
+def main():
+    cmdline.execute('scrapy crawl BookingAllSpider'.split())
 
-datahotel = os_use()
+
+def hotel_data():
+    list_of_cities = ['Port Louis Mauritius',
+    'Vacoas Mauritius',
+    'Quatre Bornes Mauritius',
+    'Rose Hill Mauritius',
+    'Blue Bay Mauritius',
+    'Flic en Flac Mauritius',
+    'Le Morne Brabant Mauritius',
+    'Trou aux Biches Mauritius',
+    'Grand Baie Mauritius']
+
+    # we define the check in date as today & the check out date as today +7
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    day_plus_7 = ((datetime.datetime.now()) + timedelta(days=7))
+    day_plus_7= day_plus_7.strftime("%Y-%m-%d")
+
+    class BookingAllSpider(scrapy.Spider):
+        # Name of your spider
+        name = "bookingAllMRU"
+
+        # Starting URL
+        start_urls = ['https://www.booking.com/']
+
+        # Parse function for form request
+        def parse(self, response):
+            for city in list_of_cities:
+            
+                yield scrapy.FormRequest.from_response(               
+                    response,
+                    formdata={ "ss": city, 
+                            "data-checkin":today,
+                            "data-checkout":day_plus_7,
+                        
+                            },
+                    callback=self.after_search
+                )
+        # Callback used after search
+        def after_search(self, response):
+            
+            
+            results = response.css('div.a1b3f50dcd.f7c6687c3d.a1f3ecff04.f996d8c258')
+                    
+            for r in results:
+                yield {
+                    'hotel_name': r.css('h3.a4225678b2 ::text').get(),
+                    'url' : r.css('h3.a4225678b2 ::attr(href)').get(),
+                    'city': r.css('span.f4bd0794db.b4273d69aa ::text').get(),
+                    'Score given by the website users': r.css('div.b5cd09854e.d10a6220b4::text').get(),
+                    'Text description of the hotel': r.css('div.d8eab2cf7f ::text').get(),
+                }
+
+                # Name of the file where the results will be saved
+    filename = "All_Booking_mru.json"
+
+    # If file already exists, delete it before crawling (because Scrapy will concatenate the last and new results otherwise)
+    if filename in os.listdir('src/'):
+            os.remove('src/' + filename)
+
+        # Declare a new CrawlerProcess with some settings
+    process = CrawlerProcess (settings = {
+        'USER_AGENT': 'Chrome/84.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'LOG_LEVEL': logging.INFO,
+        "FEEDS": {
+            'src/' + filename : {"format": "json"},
+            }
+    })
+
+        # Start the crawling using the spider you defined above
+
+    process.crawl(BookingAllSpider)
+    process.start()
+
+
+    datahotel =pd.read_json('src/All_Booking_mru.json')
+    return datahotel
+
 
 #let's scrap the hotel gps data with scrapy on booking.com
 #due to multithread issue with streamlit we have to use 'os' module
 #to put signal on the main thread
 @st.cache
-def os_use2():
-    os.system('python rungps.py')
-    return pd.read_json('src/hotel_coord_mru.json')
+def a = pd.read_json('src/All_Booking_mru.json')
+all_details = pd.DataFrame(a)
+list_of_urls = []
+for url in (all_details['url']):
+    list_of_urls.append(url)
 
-datahotelgps = os_use2()
+
+def main():
+    cmdline.execute('scrapy crawl BookingAllGeoSpider'.split())
+
+
+def load_data():
+# we scrap lat & lon for every hotel of the df:
+    class BookingAllGeoSpider(scrapy.Spider):
+        # Name of your spider
+        name = "allgeobookingmru"
+        def start_requests(self):
+
+            # Starting URL
+
+            for url in list_of_urls:
+                    yield scrapy.Request(url=url, callback=self.parse)
+    
+        def parse(self, response):
+            
+                results = response.css('#hotel_address') #on appelle le resultat via son id qui est unique
+                for r in results:
+                    yield {                               
+                        'lat_lon': r.css('::attr(data-atlas-latlng)').get()}
+                        
+
+                # Name of the file where the results will be saved
+    filename1 = "hotel_coord_mru.json"
+
+            # If file already exists, delete it before crawling (because Scrapy will concatenate the last and new results otherwise)
+    if filename1 in os.listdir('src/'):
+                    os.remove('src/' + filename1)
+
+    # Declare a new CrawlerProcess with some settings
+    process = CrawlerProcess(settings = {
+        'USER_AGENT': 'Chrome/84.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+        'LOG_LEVEL': logging.INFO,
+        "FEEDS": {
+            'src/' + filename1 : {"format": "json"},
+                }
+            })
+
+            # Start the crawling using the spider you defined above
+    
+    process.crawl(BookingAllGeoSpider)
+    process.start()
+    
+    datahotelgps =pd.read_json('src/hotel_coord_mru.json')
+    
+    return datahotelgps
 
 #let's put all datas in one well formatted df:
 @st.cache (allow_output_mutation=True)
